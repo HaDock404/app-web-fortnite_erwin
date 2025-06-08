7f8b0c8a77ec4503667b2c8f64c8aeb3ed5e3211
HaDock404 <delescluse.g@gmail.com>
2025-06-08 11:09:04 +0200

    init

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import '../styles/header.css'
+
+function Header() {
+    return (
+        <div>hello</div>
+    )
+}
+
+export default Header
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,11 @@
 import BodyHomePage from "../components/BodyHomePage"
+import Header from "../components/Header"
 import '../styles/homepage.css'
 
 function HomePage() {
     return (
         <section className="homepage">
+            <Header />
             <BodyHomePage />
         </section>
     )
