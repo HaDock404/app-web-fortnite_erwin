434730ac89a14b9ac5e5b032d442c13fcceafd31
HaDock404 <delescluse.g@gmail.com>
2025-06-11 14:28:30 +0200

    responsive done

--- src/components/Headerbis.jsx
+++ src/components/Headerbis.jsx
@@ -9,7 +9,7 @@ function Header() {
     return (
         <header>
             <nav className='header_nav'>
-                <Link to="https://www.erwin-labs.com/" className='home_logo'>
+                <Link to="/" className='home_logo'>
                     <div className='erwin_logo'>
                         <img src="https://cdn.prod.website-files.com/62d56c0bde36c5e99acfb524/648a060a4e983940f8857fb1_Group%20289660.svg" alt="" />
                     </div>
