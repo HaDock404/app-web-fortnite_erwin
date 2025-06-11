19e4acca05325e714cd7cfb66e17c002bc2c0e16
HaDock404 <delescluse.g@gmail.com>
2025-06-11 08:41:06 +0200

    responsive home logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,41 +3,24 @@ import { Link } from 'react-router-dom'; //obligatoire avec hashrouter
 import Worldicon from './Wordicon.jsx';
 import Dropdown from './Dropdown.jsx';
 
-import { useContext, useState } from 'react'; // Importation de useState pour le menu hamburger
-import { LanguageContext } from './LanguageContext';
 
 function Header() {
-    const { language } = useContext(LanguageContext);
-    const [menuOpen, setMenuOpen] = useState(false); // État pour contrôler l'affichage du menu mobile
-
-    const home = {
-        English: 'HomePage',
-        Français: "Accueil"
-    };
-
-    const abouts = {
-        English: 'About',
-        Français: 'A propos'
-    };
-
-    const uppercasedLanguage = language.toUpperCase();
-    const linkPath = `/Home_${uppercasedLanguage}`;
-
-    const toggleMenu = () => {
-        setMenuOpen(!menuOpen); // Changement de l'état du menu mobile
-    };
 
     return (
         <header>
             <nav className='header_nav'>
                 <Link to="https://www.erwin-labs.com/" className='home_logo'>
-
+                    <div className='erwin_logo'>
                         <img src="https://cdn.prod.website-files.com/62d56c0bde36c5e99acfb524/648a060a4e983940f8857fb1_Group%20289660.svg" alt="" />
+                    </div>
+                    
+                    <div className='home_logo_x'>X</div>
 
-                        <div className='home_logo_x'>X</div>
-
-                        <img src="https://cms-assets.unrealengine.com/AVzjeqAbLRKi3W5jq0CAvz/cmb81xhry3tsj07mzscwq44nf" alt="" />
+                    <div className='fortnite_logo'>
+                         <img src="https://cms-assets.unrealengine.com/AVzjeqAbLRKi3W5jq0CAvz/cmb81xhry3tsj07mzscwq44nf" alt="" />
+                    </div>      
                 </Link>
+                
                 <div className='lang-menu'>
                     <Worldicon />
                     <Dropdown />
